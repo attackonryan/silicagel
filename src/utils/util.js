b65d21a0b0b8eedfac5020976492b5bedd55dd68
attackonryan <1458727018@qq.com>
2020-04-23 11:45:59 +0800

    feat: isPlainObject method for checking an plain object

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,9 @@
 export function isObject(val) {
   return typeof val === 'object'
 }
-
+export function isPlainObject(val) {
+  return Object.prototype.toString.call(val) === "[object Object]"
+}
 export function hasOwn(val, key) {
   const hasOwnProperty = Object.prototype.hasOwnProperty
   return hasOwnProperty.call(val, key)
