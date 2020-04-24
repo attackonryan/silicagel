bdc8089f3e73dabaaabd8dd31b079b2a637784e5
attackonryan <1458727018@qq.com>
2020-04-24 16:57:41 +0800

    release: silicagel v1.1.0

--- dist/silicagel.js
+++ dist/silicagel.js
@@ -150,7 +150,7 @@
   }
 
   function compile(el, proxy) {
-    if(!proxy._isObserved){
+    if (!proxy._isObserved) {
       proxy = observe(proxy);
     }
     let fragment = document.createDocumentFragment();
@@ -171,13 +171,42 @@
               if (initSymbol === Symbol.for("replaceText")) {
                 new Watcher(proxy, placeholder, replaceText);
               }
-              const res = placeholder.split('.').reduce((val, key) => {
+              const res = placeholder.split(".").reduce((val, key) => {
                 return val[key]
               }, proxy);
               return res && res.toString && res.toString()
             });
           }
           replaceText(initSymbol);
+        }
+        if (node.nodeType === 1) {
+          const nodeAttr = node.attributes;
+          Array.from(nodeAttr).forEach(attr => {
+            let name = attr.name;
+            let exp = attr.value;
+            if (name === "bind") {
+              if (exp.includes(".")) {
+                node.value = exp.split(".").reduce((val, key) => {
+                  return val[key]
+                }, proxy);
+              }else {
+                node.value = proxy[exp];
+              }
+              new Watcher(proxy, exp, newVal => {
+                node.value = newVal;
+              });
+              node.addEventListener("input", e => {
+                console.time("test");
+                exp.split(".").reduce((val, key, i, arr) => {
+                  if (i === arr.length - 1) {
+                    val[key] = e.target.value;
+                  }
+                  return val[key]
+                }, proxy);
+                console.timeEnd("test");
+              });
+            }
+          });
         }
         if (node.childNodes && node.childNodes.length) {
           replace(node);
@@ -205,8 +234,6 @@
   };
   Silicagel.observe = observe;
   Silicagel.Watcher = Watcher;
-
-  // module.exports = Silicagel
 
   return Silicagel;
 
